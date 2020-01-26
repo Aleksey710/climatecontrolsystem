163a0d9e9973988862a50ad9fda29783872f4544
Aleksey710 <mira710@rambler.ru>
2020-01-27 00:39:50 +0200

    arm platform detect for imitator

---
+++ stable.h
@@ -0,0 +1,47 @@
+//----------------------------------------------------
+/* Add C includes here */
+
+
+//----------------------------------------------------
+#if defined __cplusplus
+/* Add C++ includes here */
+
+#include <iostream>
+#include <cassert>
+//#include <stdlib>
+#include <iostream>
+#include <vector>
+
+
+#include <QCoreApplication>
+//#include <QApplication>
+#include <QObject>
+#include <QTimer>
+#include <QDateTime>
+
+#include <QString>
+#include <QTextCodec>
+#include <QDebug>
+#include <QSystemSemaphore>
+#include <QSharedMemory>
+#include <QScopedPointer>
+#include <QString>
+
+//#include <QSqlDatabase>
+//#include <QSqlError>
+//#include <QSqlQuery>
+
+//#include <QWidget>
+//#include <QPushButton>
+//#include <QLineEdit>
+//#include <QLabel>
+//#include <QVBoxLayout>
+//#include <QHBoxLayout>
+
+//#include "Log.h"
+//#include "easylogging++.h"
+//#include "easyloggingCustom.h"
+
+
+#endif // __cplusplus
+//----------------------------------------------------

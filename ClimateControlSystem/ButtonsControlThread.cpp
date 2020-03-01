49a0a3671c29cddecd5c3f715e5a56665114a08b
Aleksey710 <mira710@rambler.ru>
2020-03-01 20:17:50 +0200

    кнопки

--- ClimateControlSystem/ButtonsControlThread.cpp
+++ ClimateControlSystem/ButtonsControlThread.cpp
@@ -28,26 +28,63 @@ void ButtonsControlThread::run()
 {
     for (;;)
     {
-        int button_1_state      = 0;
-        int button_2_state      = 0;
-        int button_3_state      = 0;
-        int button_4_state      = 0;
-        int button_off_state    = 0;
+        int bt_1_state      = 0;
+        int bt_2_state      = 0;
+        int bt_3_state      = 0;
+        int bt_4_state      = 0;
+        int bt_off_state    = 0;
 
 #ifdef __arm__
-        button_1_state = digitalRead    (BUTTON_1_PIN);
-        button_2_state = digitalRead    (BUTTON_2_PIN);
-        button_3_state = digitalRead    (BUTTON_3_PIN);
-        button_4_state = digitalRead    (BUTTON_4_PIN);
-        button_off_state = digitalRead  (BUTTON_OFF_PIN);
+        bt_1_state = digitalRead    (BUTTON_1_PIN);
+        bt_2_state = digitalRead    (BUTTON_2_PIN);
+        bt_3_state = digitalRead    (BUTTON_3_PIN);
+        bt_4_state = digitalRead    (BUTTON_4_PIN);
+        bt_off_state = digitalRead  (BUTTON_OFF_PIN);
 #endif // __arm__
 
+        //---------------------------------------------------
+        if(m_bt_1_state == 1 && bt_1_state == 0)
+            emit bt_1_pressed();
+
+        if(m_bt_2_state == 1 && bt_2_state == 0)
+            emit bt_2_pressed();
+
+        if(m_bt_3_state == 1 && bt_3_state == 0)
+            emit bt_3_pressed();
+
+        if(m_bt_4_state == 1 && bt_4_state == 0)
+            emit bt_4_pressed();
+
+        if(m_bt_off_state == 1 && bt_off_state == 0)
+            emit bt_off_pressed();
+        //---------------------------------------------------
+        if(m_bt_1_state == 0 && bt_1_state == 1)
+            emit bt_1_released();
+
+        if(m_bt_2_state == 0 && bt_2_state == 1)
+            emit bt_2_released();
+
+        if(m_bt_3_state == 0 && bt_3_state == 1)
+            emit bt_3_released();
+
+        if(m_bt_4_state == 0 && bt_4_state == 1)
+            emit bt_4_released();
+
+        if(m_bt_off_state == 0 && bt_off_state == 1)
+            emit bt_off_released();
+        //---------------------------------------------------
         qDebug() << "ButtonsControlThread"
-                 << button_1_state
-                 << button_2_state
-                 << button_3_state
-                 << button_4_state
-                 << button_off_state ;
+                 << bt_1_state
+                 << bt_2_state
+                 << bt_3_state
+                 << bt_4_state
+                 << bt_off_state ;
+        //---------------------------------------------------
+        m_bt_1_state    = bt_1_state;
+        m_bt_2_state    = bt_2_state;
+        m_bt_3_state    = bt_3_state;
+        m_bt_4_state    = bt_4_state;
+        m_bt_off_state  = bt_off_state;
 
         usleep(500);
     }
